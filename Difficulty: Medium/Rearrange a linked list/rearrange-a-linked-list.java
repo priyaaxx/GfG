af086c5662804a36ce0376cb21ba4812de224d45
Priya Gopal <114797250+priyaaxx@users.noreply.github.com>
2024-08-05 12:07:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rearrange a linked list/rearrange-a-linked-list.java
@@ -0,0 +1,103 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+This is method only submission.
+You only need to complete the below method.
+*/
+class Solution {
+
+    // Should rearrange given linked list such that all even
+    // positioned Nodes are before odd positioned.
+    // Returns nothing
+    void rearrangeEvenOdd(Node head) {
+        //  The task is to complete this method
+        Node even = new Node(-1);
+        Node odd = new Node(-1);
+        Node tEven = even;
+        Node tOdd = odd;
+        
+        Node temp = head;
+        int count = 1;
+        while(temp!=null){
+            if(count%2 == 0){
+                tEven.next = temp;
+                tEven = temp;
+                temp = temp.next;
+                 count++;
+            } else{
+                tOdd.next = temp;
+                tOdd = temp;
+                temp = temp.next;
+                 count++;
+            }
+        }
+        tOdd.next = even.next;
+        tEven.next = null;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine().trim();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+
+            Node head = null;
+            if (!arr.isEmpty()) {
+                head = new Node(arr.get(0));
+                Node tail = head;
+                for (int i = 1; i < arr.size(); i++) {
+                    tail.next = new Node(arr.get(i));
+                    tail = tail.next;
+                }
+            }
+            new Solution().rearrangeEvenOdd(head);
+            printList(head);
+        }
+    }
+}
+
+// } Driver Code Ends
